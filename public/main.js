803c80930e840227f417e1388ca9d4a037d76437
Mostafa <m.elsheikh11791@gmail.com>
2016-10-25 17:49:55 +0200

    private messaging

--- public/main.js
+++ public/main.js
@@ -2,25 +2,22 @@ $(document).ready(function() {
     var socket = io();
     var input = $('#userInput');
     var messages = $('#messages');
-    var userName = $('usernameInput');
-    
+
     var addUser = function(name) {
         $('#users').append('<div>' + name + '</div>');
     };
-    
+
     $('#join').on('click', function(event) {
         event.preventDefault();
-        var name = userName.val();
-        
+        var name = $('#usernameInput').val();
         if (name != '') {
-            console.log(name);
             socket.emit('join', name);
             $('#loginPage').hide();
             $('#mainPage').show();
             addUser(name);
         }
     });
-    
+
     var addMessage = function(message) {
         messages.append('<div>' + message + '</div>');
     };
@@ -29,13 +26,14 @@ $(document).ready(function() {
         if (event.keyCode != 13) {
             return;
         }
-        
+
         var message = input.val();
+        var tokens = message.split(':');
         addMessage(message);
         socket.emit('message', message);
         input.val('');
     });
-    
+
     socket.on('message', addMessage);
     socket.on('join', addUser);
 });
--- server.js
+++ server.js
@@ -11,24 +11,40 @@ var io = socket_io(server);
 
 var people = {};
 
+function getNameFromId(socketId) {
+  for (name in people) {
+    if (people[name] == socketId) {
+      return name;
+    }
+  }
+  return null;
+}
 
 io.on('connection', function (socket) {
-    console.log(socket.id);
-    console.log('Client connected');
-    
+
     socket.on('join', function(name) {
-        people[socket.id] = name;
-        console.log(people);
-        socket.emit('join', name);
-        console.log(name, "joined the chat");
+        // this loop goes through everyone on the server and sends a message to the connecting Client
+        // to inform them.
+        for (person in people) {
+          socket.emit('join', person);
+        }
+        people[name] = socket.id;
+        // broadcast info about connecting client to everyone else in chatroom.
+        socket.broadcast.emit('join', name);
     });
-    
+
     socket.on('message', function(message) {
-        console.log('Received message:', message);
-        socket.broadcast.emit('message', message);
+        var senderSocketId = socket.id;
+        var senderName = getNameFromId(senderSocketId);
+        var tokens = message.split(':');
+        var destinationUser = tokens[0];
+        var destinationUserID = people[destinationUser];
+        if (destinationUserID) {
+          io.to(destinationUserID).emit('message', senderName + ":" + tokens[1]);
+        }
     });
-    
+
 });
 
-  
+
 server.listen(process.env.PORT || 8080);
